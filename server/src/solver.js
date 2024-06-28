f4ab32666ba24696fc8ba52ce7cf757995456210
sandeepforgit <sandeepleela9@gmail.com>
2024-06-28 17:39:29 +0530

    second commit

--- server/src/solver.js
+++ server/src/solver.js
@@ -1,45 +1,45 @@
-
 function isSafe(board, row, col, num) {
-    for (let x = 0; x < 9; x++) {
-        if (board[row][x] === num || board[x][col] === num) return false;
-    }
+  for (let x = 0; x < 9; x++) {
+    if (board[row][x] === num || board[x][col] === num) return false;
+  }
 
-    const startRow = row - row % 3, startCol = col - col % 3;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i + startRow][j + startCol] === num) return false;
-        }
+  const startRow = row - (row % 3),
+    startCol = col - (col % 3);
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i + startRow][j + startCol] === num) return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 function solveSudoku(board) {
-    const n = board.length;
-    let emptySpot = findEmpty(board);
+  const n = board.length;
+  let emptySpot = findEmpty(board);
 
-    if (!emptySpot) return true;
+  if (!emptySpot) return true;
 
-    let [row, col] = emptySpot;
+  let [row, col] = emptySpot;
 
-    for (let num = 1; num <= 9; num++) {
-        if (isSafe(board, row, col, num)) {
-            board[row][col] = num;
-            if (solveSudoku(board)) return true;
-            board[row][col] = 0;
-        }
+  for (let num = 1; num <= 9; num++) {
+    if (isSafe(board, row, col, num)) {
+      board[row][col] = num;
+      if (solveSudoku(board)) return true;
+      board[row][col] = 0;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function findEmpty(board) {
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j] === 0) return [i, j];
-        }
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 0) return [i, j];
     }
-    return null;
+  }
+  return null;
 }
 
 module.exports = solveSudoku;
